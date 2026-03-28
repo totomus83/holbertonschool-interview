8c918ba0e5097eb0c716a798a12bd233cf7e3e2c
totomus83 <tom.dibellonio@gmail.com>
2026-03-28 18:41:51 +0100

    task 2

--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -1,3 +1,8 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stddef.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -6,10 +11,17 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
-};
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
+
+typedef struct binary_tree_s heap_t;
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+heap_t *heap_insert(heap_t **root, int value);
+
+#endif
