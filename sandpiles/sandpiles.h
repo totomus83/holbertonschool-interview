1d3974f74bc1c782aadae1aab3000eab0b7eeaaf
totomus83 <tom.dibellonio@gmail.com>
2026-04-03 18:58:35 +0200

    test

--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -1,10 +1,6 @@
 #ifndef SANDPILES_H
 #define SANDPILES_H
 
-/* Function prototypes */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-void print_grid(int grid[3][3]);
-int is_stable(int grid[3][3]);
-void topple(int grid[3][3]);
 
-#endif /* SANDPILES_H */
+#endif
