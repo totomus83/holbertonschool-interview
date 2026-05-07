622e4f331c58173046f38c9ff0db6bddbf4b3c9e
totomus83 <tom.dibellonio@gmail.com>
2026-05-07 15:16:48 +0200

    synthax

--- linear_skip/0-linear_skip.c
+++ linear_skip/0-linear_skip.c
@@ -2,69 +2,69 @@
 #include "search.h"
 
 static skiplist_t *scan_express(skiplist_t *list, int value,
-skiplist_t **prev)
+    skiplist_t **prev)
 {
-skiplist_t *curr;
+ skiplist_t *curr;
 
-curr = list->express;
+ curr = list->express;
 
-while (curr)
-{
-printf("Value checked at index [%lu] = [%d]\n",
-curr->index, curr->n);
+ while (curr)
+ {
+  printf("Value checked at index [%lu] = [%d]\n",
+   curr->index, curr->n);
 
-if (curr->n >= value)
-break;
+  if (curr->n >= value)
+   break;
 
-*prev = curr;
-curr = curr->express;
-}
+  *prev = curr;
+  curr = curr->express;
+ }
 
-return (curr);
+ return (curr);
 }
 
 static skiplist_t *linear_search(skiplist_t *start,
-skiplist_t *end, int value)
+    skiplist_t *end, int value)
 {
-skiplist_t *tmp;
+ skiplist_t *tmp;
 
-tmp = start;
+ tmp = start;
 
-while (tmp && tmp->index <= end->index)
-{
-printf("Value checked at index [%lu] = [%d]\n",
-tmp->index, tmp->n);
+ while (tmp && tmp->index <= end->index)
+ {
+  printf("Value checked at index [%lu] = [%d]\n",
+   tmp->index, tmp->n);
 
-if (tmp->n == value)
-return (tmp);
+  if (tmp->n == value)
+   return (tmp);
 
-tmp = tmp->next;
-}
+  tmp = tmp->next;
+ }
 
-return (NULL);
+ return (NULL);
 }
 
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-skiplist_t *prev, *curr, *result;
+ skiplist_t *prev, *curr, *result;
 
-if (!list)
-return (NULL);
+ if (!list)
+  return (NULL);
 
-prev = list;
-curr = scan_express(list, value, &prev);
+ prev = list;
+ curr = scan_express(list, value, &prev);
 
-if (!curr)
-{
-while (prev->next)
-prev = prev->next;
-curr = prev;
-}
+ if (!curr)
+ {
+  while (prev->next)
+   prev = prev->next;
+  curr = prev;
+ }
 
-printf("Value found between indexes [%lu] and [%lu]\n",
-prev->index, curr->index);
+ printf("Value found between indexes [%lu] and [%lu]\n",
+  prev->index, curr->index);
 
-result = linear_search(prev, curr, value);
+ result = linear_search(prev, curr, value);
 
-return (result);
+ return (result);
 }
